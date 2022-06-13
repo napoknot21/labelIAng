e5bf218c4a4b5b6257592063375bb3cef073e692
Charly Martin-Avila <kinbrian5@hotmail.com>
2022-06-13 11:34:56 +0200

    Update browserFileWindow.py

--- ui/browserFileWindow.py
+++ ui/browserFileWindow.py
@@ -4,61 +4,128 @@ from tkinter import filedialog
 class BrowserFileWindow :
 
   def __init__ (self) :
+    self.filename_video = None
+    self.filename_csv = None
     self.windowInit()
-    #self.openFileName()
-    #self.printLabelContent()
 
 
   def windowInit(self) :
     self.window = Tk()
+    self.filename = None
     self.window.title("Video-app")
     self.window.geometry("707x500")
     self.window.config(background="white")
-    self.loadLabelExplorer()
-    self.buttonsLoad()
+    self.loadLabelExplorerVideo()
+    self.loadLabelExplorerCsv()
+    #self.loadSpace()
+    self.buttonsLoadVideo()
+    self.buttonsLoadCsv()
     self.buttonPlace()
     self.window.mainloop()
 
+	
+  def loadLabelExplorerVideo (self) :
+    self.label_file_explorer_video = Label(
+        self.window,
+        text="Select a video file",
+        width=100,
+        height=5,
+        fg="blue"
+    )
 
-  def openFileName (self) :
+
+  def loadLabelExplorerCsv (self) :
+    self.label_file_explorer_csv = Label(
+        self.window,
+        text="Select a csv file",
+        width=100,
+        height=5,
+        fg="blue"
+    )  
+
+
+  def loadSpace (self) :
+    self.label_space = Label(
+        self.window,
+        width=100,
+        height= 3
+    )
+
+
+  def openFileNameVideo (self) :
     filename = filedialog.askopenfilename(
         initialdir="./",
         title="Select a Video",
         filetypes=(
             	("AVI videos", "*.avi"),
+		("mp4 videos", "*.mp4"),
             	("all files", "*.*")
         )
 	  )
-    self.filename = filename
+    self.filename_video = filename
+    self.printLabelContentVideo()
 
 
-  def loadLabelExplorer (self) :
-    self.label_file_explorer = Label(
-        self.window,
-        text="File Explorer using Tkinter",
-        width=100,
-        height=4,
-        fg="blue"
-    )
+  def openFileNameCsv (self) :
+    filename = filedialog.askopenfilename(
+        initialdir="./",
+        title="Select a Video",
+        filetypes=(
+            	("Csv files", "*.csv"),
+		("Exel files", "*.xls"),
+            	("all files", "*.*")
+        )
+	  )
+    self.filename_csv = filename
+    self.printLabelContentCsv()
 
 
-  def buttonsLoad (self) :
-    self.buttons = []
 
+  def buttonsLoadVideo (self) :
+    self.buttons_video = []
     #Create an explorer file button
     button_explorer = Button(
         self.window,
         text="Browse Files",
-        command=self.openFileName()
+        command=self.openFileNameVideo
     )
-    self.buttons.append(button_explorer)
+    self.buttons_video.append(button_explorer)
 
-    #Create a accpet button const
-    button_accept = Button(
+
+  def buttonsLoadCsv (self) :
+    self.buttons_csv = []
+    #Create an explorer file button
+    button_explorer = Button(
         self.window,
-        text="Accept"
+        text="Browse Files",
+        command=self.openFileNameCsv
     )
-    self.buttons.append(button_accept)
+    self.buttons_csv.append(button_explorer)
+
+
+
+  def printLabelContentVideo (self) :
+    self.label_file_explorer_video.configure(text="Video selected: " + self.filename_video)
+
+
+  def printLabelContentCsv (self) :
+    self.label_file_explorer_csv.configure(text="File csv selected: " + self.filename_csv)
+
+
+  def buttonPlace (self) :
+    self.label_file_explorer_video.grid(column=1, row=1)
+
+    self.label_space.grid(column=1, row=2)
+
+    for i in range (len(self.buttons_video)) :
+      self.buttons_video[i].grid(column=1, row=i+3)
+
+    self.label_space.grid(column=1, row=len(self.buttons_video)+3+1)
+
+    self.label_file_explorer_csv.grid(column=1, row=len(self.buttons_video)+3+2)
+
+    for j in range (len(self.buttons_csv)) :
+      self.buttons_csv[j].grid(column=1, row=len(self.buttons_csv)+5+1+j)
 
     #Create an exit button
     button_exit = Button(
@@ -66,24 +133,17 @@ class BrowserFileWindow :
         text="Exit",
         command=exit
     )
-    self.buttons.append(button_exit)
-
-
-  def printLabelContent (self) :
-    self.label_file_explorer.configure(text="File opened: " + self.filename)
-
-
-  def buttonPlace (self) :
-    self.label_file_explorer.grid(column=1, row=1)
-    for i in range (len(self.buttons)) :
-      self.buttons[i].grid(column=1, row=i+2)
-
+    button_exit.grid(column=1, row=len(self.buttons_csv)+6+1)
+    #self.buttons.append(button_exit)
 
   ################## Getters and Setters ##################
 
-  def getFilenamePath () :
-    return self.filename
+  def getFileNameVideoPath (self) :
+    return self.filename_video
 
+
+  def getFileNameCsvPath (self) :
+    return self.filename_csv
 
 
 # Ancient verison
