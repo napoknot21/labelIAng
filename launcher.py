c48778bf626b1b562ee4e4b0f8ab61099dba630c
Charly Martin-Avila <kinbrian5@hotmail.com>
2022-06-14 14:05:23 +0200

    Update launcher.py

---
+++ launcher.py
@@ -0,0 +1,19 @@
+import sys
+
+sys.path.insert(0,'./core')
+
+import model as md
+
+class Launcher :
+
+	#Constructor
+	def __init__(self) :
+		self.M = md.Model()
+
+#Main function 
+def main () :
+	L = Launcher()
+
+
+if __name__ == '__main__':
+	main()
