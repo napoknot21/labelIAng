ff28878f6c1afa308a8193b9d2bf236f9b5dfd1d
Charly Martin-Avila <kinbrian5@hotmail.com>
2022-06-14 14:01:21 +0200

    Update model.py

--- core/model.py
+++ core/model.py
@@ -1,5 +1,22 @@
-from video import *
-from vue import *
+import pandas as pd
+import os, sys
 
-filename = getFileName()
-v = Video(filename)
+sys.path.insert(0,'./ui')
+
+import browserFileWindow as br
+import signalsWindow as sw
+
+sys.path.insert(0,'./core')
+
+import connexion as cn
+
+class Model :
+
+    #Constructor
+    def __init__(self) :
+        data = br.BrowserFileWindow().getFileNameVideoAndCsvPath()
+        self.filename_video = data[0] #filename_video
+        self.filename_csv = data[1] #filename_csv
+        self.T = cn.Connexion(self.filename_video, self.filename_csv)
+        self.SW = sw.SignalsWindow(self.filename_csv)
+        
