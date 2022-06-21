4bc210ff408b9aed5d9423ecfeeef1b68b20fae4
napoknot21 <kinbrian5@hotmail.com>
2022-06-21 17:35:40 +0200

    fixes and improvements

--- core/model.py
+++ core/model.py
@@ -1,20 +1,13 @@
 import pandas as pd
-import os, sys
-
-sys.path.insert(0,'./ui')
-
-import browserFileWindow as br
-import signalsWindow as sw
-
-sys.path.insert(0,'./core')
-
-import connexion as cn
+from ui import browserFileWindow as br
+from ui import signalsWindow as sg
+from core import connexion as cn
 
 class Model :
 
     #Constructor
     def __init__(self) :
-        data = br.BrowserFileWindow().getFileNameVideoAndCsvPath()
+        data = br.BrowserFileWindow().getFileVideoAndCsvPath()
         self.filename_video = data[0] #filename_video
         self.filename_csv = data[1] #filename_csv
         self.T = cn.Connexion(self.filename_video, self.filename_csv)
