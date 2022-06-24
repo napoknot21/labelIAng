3afeb4026679eb324c4706f509ac4f0d121f08f7
napoknot21 <kinbrian5@hotmail.com>
2022-06-24 14:12:12 +0200

    Add of canvas and labels for the mainWindow.py

--- ui/mainWindow.py
+++ ui/mainWindow.py
@@ -1,6 +1,7 @@
 from tkinter import *
 
-class MainWindow :
+
+class MainWindow:
 
     def __init__(self, signals_selected=None, filename_video=None, labels_entered=None):
         self.signals_selected = signals_selected
@@ -9,7 +10,11 @@ class MainWindow :
         self.window = Tk()
         self.initWindow()
         self.loadAndPlaceMainLabels()
+        self.loadSubLabelsHeader()
+        self.loadAndPlaceSubLabelsHeader()
+        self.loadAndPlaceSignalsLabels(3)
         self.window.mainloop()
+
 
     def initWindow(self):
         self.window.title("Video app 1.0")
@@ -26,7 +31,7 @@ class MainWindow :
 
 
     # Load header label
-    def loadHeader (self) :
+    def loadHeader(self):
         self.header = Label(
             self.window,
             bg="black"
@@ -35,7 +40,7 @@ class MainWindow :
 
 
     # Load body label
-    def loadBody (self) :
+    def loadBody(self):
         self.body = Label(
             self.window,
             bg="blue"
@@ -47,24 +52,62 @@ class MainWindow :
     def loadAndPlaceMainLabels(self):
         self.loadHeader()
         self.loadBody()
-        self.loadSubLabelsHeader()
 
 
-    def loadSubLabelsHeader (self) :
-        self.video_label = Label (
+    def loadSubLabelsHeader(self):
+        self.video_label = Label(
             self.header,
             bg="yellow"
         )
         self.video_label.place(relx=0, rely=0, relheight=1, relwidth=.65)
-        self.labels_label = Label (
+        self.labels_label = Label(
             self.header,
             bg="green"
         )
         self.labels_label.place(relx=.65, rely=0, relheight=1, relwidth=.35)
 
 
+    def loadAndPlaceSubLabelsHeader(self):
+        self.loadAndPlaceVideoCanvas()
+        self.loadAndPlaceLabelsCanvas()
 
 
+    def loadAndPlaceVideoCanvas(self):
+        self.video_canvas = Canvas(
+            self.video_label,
+            bg="white"
+        )
+        self.video_canvas.place(relx=0.05, rely=0.05, relwidth=0.905, relheight=0.9)
 
 
-        # ((1920 / 1080) * .8)
+    def loadAndPlaceLabelsCanvas(self):
+        self.video_info = Label(
+            self.labels_label,
+            bg="brown"
+        )
+        self.video_info.place(relx=.05, rely=.05, relwidth=.905, relheight=.15)
+        self.labels_canvas = Label(
+            self.labels_label,
+            bg="brown"
+        )
+        self.labels_canvas.place(relx=.05, rely=.25, relwidth=.905, relheight=.5)
+        self.buttons_canvas = Label(
+            self.labels_label,
+            bg="brown"
+        )
+        self.buttons_canvas.place(relx=.05, rely=.8, relwidth=.905, relheight=.15)
+
+
+    def loadAndPlaceSignalsLabels (self, signalsLength):
+        self.signals = []
+        space = 0.01
+        rely = 0
+        height = (1 - ((signalsLength + 1) * space)) / signalsLength
+        for i in range(signalsLength):
+            signal_label = Label (
+                self.body,
+                bg="pink"
+            )
+            signal_label.place(relx=0.003, rely=space + rely, relwidth=.985, relheigh=height)
+            space += space
+            rely += height
