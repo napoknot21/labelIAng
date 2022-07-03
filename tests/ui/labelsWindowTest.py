1a87411924e17a700a3dc694a2f4d30b7e352625
napoknot21 <kinbrian5@hotmail.com>
2022-07-03 16:16:15 +0200

    add of test file

---
+++ tests/ui/labelsWindowTest.py
@@ -0,0 +1,18 @@
+import timer, os, sys, logging
+from tkinter import *
+
+userin = ""
+
+root = Tk()
+root.geometry('1080x660')
+root.title('Terminal')
+root.configure(bg="black")
+
+e = Entry(root, textvariable=userin, fg='lime', bg='black')
+e.grid()
+
+def process (event=None) :
+    content = e.get()
+    print(content)
+
+e.bind('<Return>', process)
