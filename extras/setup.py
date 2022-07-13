52f56011550553b7eb65716a2401e376e8895a4a
napoknot21 <kinbrian5@hotmail.com>
2022-07-13 11:58:57 +0200

    setup.py update packages

--- extras/setup.py
+++ extras/setup.py
@@ -1,3 +1,22 @@
-# Set up file for the launcher.py 
+import importlib.util
+import sys, subprocess
 
-import sys, os, subprocess
+libraries = ['pandas', 'opencv-python', 'pillow', 'tk', 'matplotlib']
+names = ['pandas', 'cv2', 'PIL', 'tk', 'matplotlib',]
+
+for i, name in enumerate(names) :
+
+    if name in sys.modules:
+        print(f"{name!r} already in sys.modules")
+    elif (spec := importlib.util.find_spec(name)) is not None:
+        # If you choose to perform the actual import ...
+        module = importlib.util.module_from_spec(spec)
+        sys.modules[name] = module
+        spec.loader.exec_module(module)
+        print(f"{name!r} has been imported")
+    else:
+        #index.append(i)
+        print(f"can't find the {name!r} module")
+        subprocess.check_call([sys.executable, '-m', 'pip', 'install', libraries[i]])
+
+print("All packages and libraries needed are imported/installed !")
--- launcher.py
+++ launcher.py
@@ -1,26 +1,20 @@
-import matplotlib
-from src.core import model as md
-from src.ui import mainWindow as mw
-from src.ui import labelsWindow as lw
 import sys, subprocess
+subprocess.check_call([sys.executable, 'extras/setup.py'])
+
+from src.core import model as md
+#from src.ui import mainWindow as mw
+#from src.ui import labelsWindow as lw
+
 
 class Launcher :
 
 	#Constructor
 	def __init__(self) :
-		self.libraries = ['pandas', 'opencv', 'matplotlib', 'tk', 'pillow']
-		#self.installPackages()
 		self.M = md.Model()
 		#self.LW = lw.LabelsWindow()
 		#self.MW = mw.MainWindow(signals_selected=None, labels_entered=None)
 
 
-	def installPackages (self) :
-		subprocess.check_call([sys.executable, '-m', 'pip', 'intall', 'pandas'])
-		"""for libray in self.libraries :
-			subprocess.check_call([sys.executable, "-m", "pip", "intall", libray])"""
-
-		
 
 #Main function 
 def main () :
