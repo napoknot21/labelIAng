0262c000a616763b6abcbf8ab2de5e9c2f0b5ab6
napoknot21 <kinbrian5@hotmail.com>
2022-07-27 19:10:38 +0200

    documentation for labelUI.py

--- src/ui/assets/labelUI.py
+++ src/ui/assets/labelUI.py
@@ -1,17 +1,37 @@
 from src.core import label as lb
 from tkinter import *
-import matplotlib as mt
 
 
 class LabelUI :
 
+    # Constructor
     def __init__ (self, master, label):
+        """
+        Constructor for the LabelUI object
+
+        Parameters
+        ----------
+            master : Tkinter object
+                A Tkinter/Canvas where the graphical label (label.py) should be placed
+
+            label : Label object
+                The label to be placed on the master
+        """
         self.master = master
         self.label = label
 
 
     def loadLabelBlock (self) :
+        """
+        Loader function for the graphical Label block
+
+        Notes
+        -----
+            The choice of using a canvas instead of a label is determined by the visualization:
+            When scrolling, a label could be not visible on the screen
+        """
         label_block = Canvas ( self.master,height=100)
+        # id label
         id_label = Label ( label_block, text="ID: " + str(self.label.getId()))
         id_label.place(relwidth=.2, relheight=1, relx=0, rely=0)
         # Specific label for the name block
@@ -25,10 +45,12 @@ class LabelUI :
         return label_block
 
     
-    # Getter for the master widget
+    # Getter
     def getMaster (self) :
+        """Getter fot the master widget """
         return self.master
 
-    # Getter for the label object
+    # Getter
     def getLabel (self) :
+        """Getter for the label widget"""
         return self.label
